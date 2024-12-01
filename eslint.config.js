c279d34cdd08d49d974d2902ee9ca1ee50d19ad3
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-01 14:41:55 -0300

    adding all configuration files

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+export default [
+  {
+    ignores: ["node_modules"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      react: require("eslint-plugin-react"),
+      prettier: require("eslint-plugin-prettier"),
+      import: require("eslint-plugin-import"),
+      "react-hooks": require("eslint-plugin-react-hooks"),
+    },
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-use-before-define": "off",
+      "@typescript-eslint/no-use-before-define": ["error"],
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+      "import/extensions": "off",
+      "no-shadow": "off",
+      "@typescript-eslint/no-shadow": ["error"],
+      "@typescript-eslint/explicit-function-return-type": [
+        "error",
+        {
+          allowExpressions: true,
+        },
+      ],
+      "max-len": [
+        "warn",
+        {
+          code: 100,
+          ignoreComments: true,
+          ignoreUrls: true,
+        },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "import/prefer-default-export": "off",
+      "react/prop-types": "off",
+      "prettier/prettier": ["error", { endOfLine: "auto", singleQuote: false }],
+      quotes: ["error", "double"],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+  },
+];
