54f6d44c96f1bbeedb6b203722dc532938978683
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-01 14:39:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  return (
+    <>
+      <div className="h-24 w-24 bg-teal-950 flex justify-center items-center">
+        <h1 className="text-blue-300">Hello World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
