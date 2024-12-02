a6a738d282e0f10b57108a172f343d2db8ece922
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-02 20:32:18 -0300

    addition of custom colors to the tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-purple": "#633cff",
+        "custom-light-purple": "#beadff",
+        "custom-lavender": "#efebff",
+        "custom-black": "#333333",
+        "custom-gray": "#737373",
+        "custom-light-gray": "#d9d9d9",
+        "custom-off-white": "#fafafa",
+        "custom-white": "#ffffff",
+        "custom-red": "#ff3939",
+      },
+      borderWidth: {
+        1.5: "1.5px",
+      },
+      width: {
+        120: "476px",
+      },
+    },
   },
   plugins: [],
 };
