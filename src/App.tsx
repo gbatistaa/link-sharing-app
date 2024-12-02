f88f4ab300a6344fee54ee3350cc4fee650d6652
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-02 20:33:15 -0300

    current component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Login from "./components/Login";
+import "./css/tailwind.css";
+
 function App(): JSX.Element {
   return (
-    <>
-      <div className="h-24 w-24 bg-teal-950 flex justify-center items-center">
-        <h1 className="text-blue-300">Hello World!</h1>
-      </div>
-    </>
+    <div className="h-auto w-auto">
+      <Login />
+    </div>
   );
 }
 
