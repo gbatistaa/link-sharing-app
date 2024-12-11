2e1569a1a1850c9afbc7361af6e5b1676c17c01e
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-11 17:54:59 -0300

    addition of react routes on the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import Menu from "./components/Menu";
 import "./css/tailwind.css";
 
 function App(): JSX.Element {
   return (
-    <div className="h-auto w-auto">
-      <Login />
+    <div className="h-auto w-auto transition-all ease-out duration-200">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+          <Route path="/" element={<Navigate to="/menu" />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
