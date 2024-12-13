164c8d813fdde552133e35aabbc24572a74220ec
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-13 08:05:43 -0300

    circumstancial component for links list is empty

---
+++ src/components/Menu/components/Links/Empty.tsx
@@ -0,0 +1,15 @@
+import empty from "../../../../../assets/images/illustration-empty.svg";
+
+function Empty(): JSX.Element {
+  return (
+    <div className="bg-custom-off-white flex flex-auto justify-center items-center flex-col rounded-md gap-y-6">
+      <img src={empty} />
+      <h1 className="text-3xl font-bold">Let's get you started</h1>
+      <p className="text-center w-3/5 text-sm text-custom-gray">
+        Use the "Add new link" button to get started. Once you have more than one link, you can reorder and edit them. We're here to help you share your profiles with everyone!
+      </p>
+    </div>
+  );
+}
+
+export default Empty;
