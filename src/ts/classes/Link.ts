15fee10f6bab142a36b819ba788c2fb2b9a33e1e
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-16 10:38:17 -0300

    removing empty space from Stackoverflow link string

--- src/ts/classes/Link.ts
+++ src/ts/classes/Link.ts
@@ -1,4 +1,4 @@
-export type Plataform = "GitHub" | "Twitter" | "LinkedIn" | "YouTube" | "Facebook" | "Twitch" | "Dev.to" | "Codewars" | "freeCodeCamp" | "GitLab" | "Hashnode" | "Stack Overflow";
+export type Plataform = "GitHub" | "Twitter" | "LinkedIn" | "YouTube" | "Facebook" | "Twitch" | "Dev.to" | "Codewars" | "freeCodeCamp" | "GitLab" | "Hashnode" | "StackOverflow";
 
 export default class UserLink {
   public url: string;
