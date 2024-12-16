4aa0da2d703337e955e194a72f222d840182aec0
gbatistadevgbatistadev <gbatistadev@gmail.com>
2024-12-16 23:39:36 +0000

    reorganized tailwindcss classes and changed list Atom to atomFamily

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,4 +1,6 @@
 import { atom } from "jotai";
+import { atomFamily } from "jotai/utils";
+import { PrimitiveAtom } from "jotai/vanilla";
 import { Link } from "react-router-dom";
 import links from "../../../assets/images/icon-links-header.svg";
 import profile from "../../../assets/images/icon-profile-details-header.svg";
@@ -7,28 +9,28 @@ import logo from "../../../assets/images/logo-devlinks-large.svg";
 import UserLink from "../../ts/classes/Link";
 import CustomizeLinks from "./components/Links/CustomizeLinks";
 
-export const linksAtom = atom<UserLink[]>([new UserLink(0)]);
+export const linksAtomFamily = atomFamily((linkId: number): PrimitiveAtom<UserLink> => {
+  return atom<UserLink>(new UserLink("", "GitHub", linkId));
+});
 
 function Menu(): JSX.Element {
   console.log(document.body.innerHTML);
 
   return (
-    <div className="w-screen h-screen p-4 flex flex-col bg-custom-off-white gap-y-4">
-      <header className="w-full h-auto p-4 flex justify-between items-center bg-white rounded-md">
+    <div className="flex flex-col gap-y-4 p-4 w-screen h-screen bg-custom-off-white">
+      <header className="flex justify-between items-center p-4 w-full h-auto bg-white rounded-md">
         <img src={logo} className="w-auto h-7" alt="Logo" />
-        <div className="flex justify-between w-60 gap-x-4">
+        <div className="flex gap-x-4 justify-between w-60">
           <Link
             to="links"
-            className="flex justify-center items-center text-sm bg-custom-lavender
-           text-custom-purple font-semibold text-nowrap w-fit px-5 gap-x-1 h-10 rounded-md"
+            className="flex gap-x-1 justify-center items-center px-5 h-10 text-sm font-semibold rounded-md bg-custom-lavender text-custom-purple text-nowrap w-fit"
           >
             <img src={links} className="w-auto h-5" alt="Links Icon" />
             Links
           </Link>
           <Link
             to="profile-details"
-            className="flex justify-center items-center text-sm bg-custom-lavender
-           text-custom-purple font-semibold text-nowrap w-fit px-5 gap-x-1 h-10 rounded-md"
+            className="flex gap-x-1 justify-center items-center px-5 h-10 text-sm font-semibold rounded-md bg-custom-lavender text-custom-purple text-nowrap w-fit"
           >
             <img src={profile} className="w-auto h-5" alt="Profile Icon" />
             Profile Details
@@ -42,9 +44,9 @@ function Menu(): JSX.Element {
           Preview
         </button>
       </header>
-      <main className="grid grid-cols-5 h-full gap-x-4">
-        <aside className="bg-white h-full flex justify-center items-center col-span-2 p-4 rounded-md">
-          <img src={phone} className="h-fit w-1/2" />
+      <main className="grid grid-cols-5 gap-x-4 h-full">
+        <aside className="flex col-span-2 justify-center items-center p-4 h-full bg-white rounded-md">
+          <img src={phone} className="w-1/2 h-fit" />
         </aside>
         <CustomizeLinks />
       </main>
