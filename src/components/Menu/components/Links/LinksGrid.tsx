7b2051da8e2d5bfdcb2a19c11608b96a8a942ee2
gbatistadevgbatistadev <gbatistadev@gmail.com>
2024-12-16 23:45:47 +0000

    realocation of the plataform link choosing component to a seperated component

--- src/components/Menu/components/Links/LinksGrid.tsx
+++ src/components/Menu/components/Links/LinksGrid.tsx
@@ -1,53 +1,28 @@
 import { atom, useAtom } from "jotai";
-import dropdown from "../../../../../assets/images/icon-chevron-down.svg";
 import drop from "../../../../../assets/images/icon-drag-and-drop.svg";
 import linkIcon from "../../../../../assets/images/icon-link.svg";
-import plataforms from "../../../../ts/lists/plataformsList";
-import { linksAtom } from "../../Menu";
-import KeyValuePlataforms from "./keyValuePlataforms";
+import PlataformChooser from "./PlataformChooser";
 
 export const dropAtom = atom<boolean>(false);
+export const linksCountAtom = atom<number[]>([]);
 
 function LinksGrid(): JSX.Element {
-  const [links] = useAtom(linksAtom);
-  const [isDropOn, setIsDropOn] = useAtom(dropAtom);
+  const [links] = useAtom(linksCountAtom);
 
   return (
-    <div className="overflow-y-auto h-84 flex flex-col gap-y-6 z-10">
+    <div className="flex overflow-y-auto z-10 flex-col gap-y-6 p-4 h-84">
       {links.map((link, linkIndex) => (
-        <article key={linkIndex} className="bg-blue-200 w-full h-fit max-h-52 flex flex-auto justify-center flex-col rounded-md gap-y-3 p-4">
-          <section className="h-fit w-full flex justify-between">
+        <article key={linkIndex} className="flex flex-col flex-auto gap-y-3 justify-center p-4 w-full max-h-52 rounded-md bg-custom-off-white h-fit">
+          <section className="flex justify-between w-full h-fit">
             <div className="flex justify-between items-center w-16">
               <img src={drop} className="" />
-              <p className="text-sm text-custom-gray font-bold">Link #{linkIndex + 1}</p>
+              <p className="text-sm font-bold text-custom-gray">Link #{linkIndex + 1}</p>
             </div>
             <button className="text-sm text-custom-gray">Remove</button>
           </section>
           <div className="flex flex-col gap-y-1">
             <p className="text-xs text-custom-gray">Platform</p>
-            <button
-              type="button"
-              className="h-10 bg-white border-custom-light-gray group focus-within:border-custom-purple border-solid border-1.5
-                rounded-md w-full flex justify-between items-center relative"
-            >
-              <div>
-                <img src={""} />
-                <p>{link.plataform}</p>
-              </div>
-              <img src={dropdown} className="group-focus:rotate-0 rotate-180 mr-4" />
-              <div
-                className="h-fit w-full absolute top-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg
-                z-20 flex-col overflow-y-visible grid grid-cols-2"
-              >
-                {plataforms.map((plataform, idx) => (
-                  <label key={idx} className="px-4 py-2 cursor-pointer hover:bg-gray-200 flex flex-row gap-x-2 items-center">
-                    <input type="radio" name={`plataform-${linkIndex}`} value={plataform} className="hidden" />
-                    {plataform}
-                    <KeyValuePlataforms plataform={plataform} selected={true} />
-                  </label>
-                ))}
-              </div>
-            </button>
+            <PlataformChooser linkId={linkIndex + 1} />
           </div>
           <div className="flex flex-col gap-y-1">
             <p className="text-xs text-custom-gray">Links</p>
@@ -56,7 +31,7 @@ function LinksGrid(): JSX.Element {
                 border-solid border-1.5 rounded-md w-full flex px-4 gap-x-2"
             >
               <img src={linkIcon} />
-              <input type="text" className="bg-transparent outline-none w-full" />
+              <input type="text" className="w-full bg-transparent outline-none" />
             </label>
           </div>
         </article>
