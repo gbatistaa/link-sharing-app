7df88cbe3a80e6908b6dd6f57f3dbf5e36a3cace
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-16 08:40:19 -0300

    condfiguration class for user links

---
+++ src/ts/classes/Link.ts
@@ -0,0 +1,10 @@
+export type Plataform = "GitHub" | "Twitter" | "LinkedIn" | "YouTube" | "Facebook" | "Twitch" | "Dev.to" | "Codewars" | "freeCodeCamp" | "GitLab" | "Hashnode" | "Stack Overflow";
+
+export default class UserLink {
+  public url: string;
+  public plataform: Plataform;
+  constructor(url: string, plataform: Plataform) {
+    this.url = url;
+    this.plataform = plataform;
+  }
+}
