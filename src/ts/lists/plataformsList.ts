4146be7dcc1b2d367369781040e8a8400a959f4c
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-16 08:42:21 -0300

    possible plataforms links list

---
+++ src/ts/lists/plataformsList.ts
@@ -0,0 +1,5 @@
+import { Plataform } from "../classes/Link";
+
+const plataforms: Plataform[] = ["GitHub", "Twitter", "LinkedIn", "YouTube", "Facebook", "Twitch", "Dev.to", "Codewars", "freeCodeCamp", "GitLab", "Hashnode", "Stack Overflow"];
+
+export default plataforms;
