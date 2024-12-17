6d315cb294de7c4c3069e68b0907c015f528e0a7
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-16 22:32:17 -0300

    changing de section to a form and eslint formatation

--- src/components/Menu/components/Links/CustomizeLinks.tsx
+++ src/components/Menu/components/Links/CustomizeLinks.tsx
@@ -1,4 +1,5 @@
 import { useAtom } from "jotai";
+import Empty from "./Empty";
 import LinksGrid, { linksCountAtom } from "./LinksGrid";
 
 function CustomizeLinks(): JSX.Element {
@@ -9,31 +10,34 @@ function CustomizeLinks(): JSX.Element {
   };
 
   return (
-    <section className="flex flex-col col-span-3 p-8 max-h-full bg-white rounded-md">
+    <form className="flex flex-col col-span-3 p-8 max-h-full bg-white rounded-md">
       <div className="flex flex-col gap-y-4 h-full bg-white">
         <h1 className="text-3xl font-bold">Customize your links</h1>
-        <p className="text-sm text-custom-gray">Add/edit/remove links below and then share all your profiles with the world!</p>
+        <p className="text-sm text-custom-gray">
+          Add/edit/remove links below and then share all your profiles with the world!
+        </p>
         <button
           type="button"
           className="flex justify-center items-center text-sm border-custom-purple border-solid border-1.5
-           text-custom-purple font-semibold text-nowrap hover:bg-custom-lavender px-5 gap-x-1 rounded-md w-full h-8 min-h-8"
+        text-custom-purple font-semibold text-nowrap hover:bg-custom-lavender px-5 gap-x-1 rounded-md
+          w-full h-8 min-h-8"
           onClick={() => incrementLinks()}
         >
           + Add new link
         </button>
-        {linksIds.length === 0 && <LinksGrid />}
+        {linksIds.length === 0 && <Empty />}
         {linksIds.length > 0 && <LinksGrid />}
       </div>
       <footer className="flex justify-end items-end h-16">
         <button
-          type="button"
+          type="submit"
           className="flex justify-center items-center text-sm bg-custom-purple border-solid border-1.5
             text-white font-semibold w-fit h-10 hover:bg-custom-light-purple px-5 gap-x-1  rounded-md"
         >
           Save
         </button>
       </footer>
-    </section>
+    </form>
   );
 }
 
