5316762c1797b72689ab1490e392ea550152ea3f
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-16 22:30:29 -0300

    user link class eslint formatation

--- src/ts/classes/Link.ts
+++ src/ts/classes/Link.ts
@@ -1,10 +1,23 @@
-export type Plataform = "GitHub" | "Twitter" | "LinkedIn" | "YouTube" | "Facebook" | "Twitch" | "Dev.to" | "Codewars" | "freeCodeCamp" | "GitLab" | "Hashnode" | "StackOverflow";
+export type Plataform =
+  | "GitHub"
+  | "Twitter"
+  | "LinkedIn"
+  | "YouTube"
+  | "Facebook"
+  | "Twitch"
+  | "Dev.to"
+  | "Codewars"
+  | "freeCodeCamp"
+  | "GitLab"
+  | "Hashnode"
+  | "StackOverflow"
+  | "";
 
 export default class UserLink {
   public url: string;
   public plataform: Plataform;
   public id: number;
-  constructor(url: string, plataform: Plataform, id:number) {
+  constructor(url: string, plataform: Plataform, id: number) {
     this.url = url;
     this.plataform = plataform;
     this.id = id;
