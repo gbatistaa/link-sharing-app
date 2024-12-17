1aaf3f39033c6625d4b4798d39e11684a4bc232e
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-16 22:33:48 -0300

    adding the fucntionality of updating the plataform value of a link

--- src/components/Menu/components/Links/PlataformChooser.tsx
+++ src/components/Menu/components/Links/PlataformChooser.tsx
@@ -1,13 +1,21 @@
 import { useAtom } from "jotai";
+import React from "react";
 import dropdown from "../../../../../assets/images/icon-chevron-down.svg";
+import { Plataform } from "../../../../ts/classes/Link";
 import plataforms from "../../../../ts/lists/plataformsList";
-
 import { linksAtomFamily } from "../../Menu";
 import KeyValuePlataforms from "./KeyValuePlataforms";
 
 function PlataformChooser({ linkId }: { linkId: number }): JSX.Element {
-
   const [link, setLink] = useAtom(linksAtomFamily(linkId));
+
+  const handlePlataformSelection = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const newPlataform = event.target.value as Plataform;
+    setLink((currLink) => ({
+      ...currLink,
+      plataform: newPlataform,
+    }));
+  };
 
   return (
     <button
@@ -15,24 +23,34 @@ function PlataformChooser({ linkId }: { linkId: number }): JSX.Element {
       className="h-10 bg-white border-custom-light-gray group focus-within:border-custom-purple border-solid border-1.5
                 rounded-md w-full flex justify-between items-center relative"
     >
-      <div>
-        <img src={""} />
-        <p className="ml-4">{link.plataform}</p>
+      <div className="ml-4 flex flex-row gap-x-2 justify-between items-center">
+        <KeyValuePlataforms plataform={link.plataform} selected={false} />
+        <p className="text-custom-black">{link.plataform}</p>
       </div>
       <img src={dropdown} className="mr-4 rotate-180 group-focus:rotate-0" />
       <div
-        className="grid overflow-y-visible absolute top-full z-20 flex-col grid-cols-2 mt-1 w-full bg-white rounded-md h-fit"
+        className="group-focus:grid hidden overflow-y-visible group-focus:absolute top-full z-20 grid-cols-2
+        mt-1 w-full bg-white rounded-md h-fit border-1.5 border-solid border-white"
       >
         {plataforms.map((plataform, idx) => (
-          <label key={idx} className="hidden flex-row gap-x-2 items-center px-4 py-2 cursor-pointer hover:bg-gray-200">
-            <input type="radio" name={`plataform-${linkId}`} value={plataform} className="" />
-            {plataform}
-            <KeyValuePlataforms plataform={plataform} selected={false} />
+          <label key={idx} className="flex flex-row gap-x-2 items-center px-4 py-2 cursor-pointer hover:bg-gray-200">
+            <input
+              type="radio"
+              name={`plataform-${linkId}`}
+              value={plataform}
+              checked={link.plataform === plataform}
+              onChange={(e) => handlePlataformSelection(e)}
+              className=""
+            />
+            <span className={`${link.plataform === plataform ? "text-custom-purple" : "text-custom-black"}`}>
+              {plataform}
+            </span>
+            <KeyValuePlataforms plataform={plataform} selected={link.plataform === plataform} />
           </label>
         ))}
       </div>
     </button>
-  )
+  );
 }
 
 export default PlataformChooser;
