cd972257f0099b1ab2a38e7944bb54818c1cee00
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-18 22:19:39 -0300

    creation of the user credentials typescript class

---
+++ src/ts/classes/UserCredentials.ts
@@ -0,0 +1,10 @@
+export default class UserCredentials {
+  public firstName: string;
+  public lastName: string;
+  public email: string;
+  constructor(firstName: string, lastName: string, email: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+}
