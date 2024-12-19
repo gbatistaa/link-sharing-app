287f81a7a3eb57a388b5160e1de43075975afd46
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-19 08:48:28 -0300

    using auxiliar atom to create a setting function to the profile picture atomWithStorage

--- src/components/Menu/components/Profile/ProfileDetails.tsx
+++ src/components/Menu/components/Profile/ProfileDetails.tsx
@@ -1,15 +1,14 @@
-import { useAtom } from "jotai";
+import { atom, useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import React, { useEffect } from "react";
 import UploadImageIcon from "../../../../../assets/images/icon-upload-image";
-import UserCredentials from "../../../../ts/classes/UserCredentials";
 import { ProfilePicture } from "../../../../ts/classes/ProfilePicture";
-
-
+import UserCredentials from "../../../../ts/classes/UserCredentials";
 
 // Atom para o estado da imagem
-export const pictureAtom = atomWithStorage<ProfilePicture>("profilePicture, {
-});
+const storageAtom = atomWithStorage<ProfilePicture>("profilePicture", { name: "", size: 0, type: "", url: null });
+
+const pictureAtom = atom<ProfilePicture>((get) => { }, (get, set) =>);
 
 // Atom para os dados do usuário
 export const userCredentialsAtom = atomWithStorage<UserCredentials>("userCredentials", {
@@ -22,13 +21,11 @@ function ProfileDetails(): JSX.Element {
   const [profilePicture, setProfilePicture] = useAtom(pictureAtom);
   const [, setUserCredentials] = useAtom(userCredentialsAtom);
 
-  // Gerenciar a mudança da imagem
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     if (event.target.files && event.target.files[0]) {
       const newProfilePicture = event.target.files[0];
       const newProfilePictureUrl = URL.createObjectURL(newProfilePicture);
 
-      // Salvar dados do arquivo no estado
       setProfilePicture({
         name: newProfilePicture.name,
         size: newProfilePicture.size,
@@ -45,7 +42,6 @@ function ProfileDetails(): JSX.Element {
     }
   };
 
-  // Atualizar dados do usuário
   const handleUserCredentialsUpdate = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target;
     setUserCredentials((prevCred) => ({
@@ -54,23 +50,20 @@ function ProfileDetails(): JSX.Element {
     }));
   };
 
-  // Gerar estilos dinamicamente
   const backgroundImageStyle = React.useMemo(() => {
-    // Resgata o item do localStorage
     const storedPicture = localStorage.getItem("profilePicture");
 
-    // Verifica se há algo salvo no localStorage
     if (storedPicture) {
       try {
-        const parsedPicture = JSON.parse(storedPicture); // Converte para objeto
-        return parsedPicture.url ? `url(${parsedPicture.url})` : undefined; // Retorna a URL se existir
+        const parsedPicture = JSON.parse(storedPicture);
+        return parsedPicture.url ? `url(${parsedPicture.url})` : undefined;
       } catch (error) {
         console.error("Erro ao parsear o profilePicture:", error);
-        return undefined; // Caso dê erro, retorna undefined
+        return undefined;
       }
     }
 
-    return undefined; // Caso não exista no localStorage, retorna undefined
+    return undefined;
   }, []);
   const fillColor = profilePicture.url ? "#ffffff" : "#633cff";
   const buttonText = profilePicture.url ? "Change Image" : "+Upload Image";
@@ -83,77 +76,43 @@ function ProfileDetails(): JSX.Element {
   return (
     <div className="flex flex-col col-span-3 gap-y-4 p-8 w-full h-full max-h-full bg-white rounded-lg">
       <h1 className="text-3xl font-bold">Profile Details</h1>
-      <p className="text-sm text-custom-gray">
-        Add your details to create a personal touch to your profile.
-      </p>
+      <p className="text-sm text-custom-gray">Add your details to create a personal touch to your profile.</p>
       <section className="flex justify-between items-center p-4 h-2/5 rounded-md bg-custom-off-white">
         <p className="text-sm text-custom-gray">Profile Picture</p>
         <label
           htmlFor="profilePicture"
-          className={`flex flex-col bg-center bg-cover gap-y-2 justify-center items-center h-full rounded-lg cursor-pointer aspect-square font-semibold text-sm ${profilePicture.url ? "bg-cover" : "bg-custom-lavender"
-            }`}
+          className={`flex flex-col bg-center bg-cover gap-y-2 justify-center items-center h-full rounded-lg cursor-pointer aspect-square font-semibold text-sm ${profilePicture.url ? "bg-cover" : "bg-custom-lavender"}`}
           style={{
             backgroundImage: backgroundImageStyle,
           }}
         >
           <UploadImageIcon fillColor={fillColor} />
           <p className={textColor}>{buttonText}</p>
-          <input
-            type="file"
-            id="profilePicture"
-            className="hidden"
-            onChange={(e) => handleImageChange(e)}
-          />
+          <input type="file" id="profilePicture" className="hidden" onChange={(e) => handleImageChange(e)} />
         </label>
         <div className="flex flex-col">
           <p className="text-xs text-custom-gray">Image must be below 1024x1024.</p>
           <p className="text-xs text-custom-gray">Use PNG or JGP format.</p>
 
-          <p className="text-xs text-custom-gray">
-            {`File: ${profilePicture.name}, Size: ${(profilePicture.size / 1024).toFixed(
-              2
-            )} KB, Type: ${profilePicture.type} ${localStorage.getItem("profilePicture")}`}
-          </p>
-
+          <p className="text-xs text-custom-gray">{`File: ${profilePicture.name}, Size: ${(profilePicture.size / 1024).toFixed(2)} KB, Type: ${profilePicture.type} ${localStorage.getItem("profilePicture")}`}</p>
         </div>
       </section>
       <section className="flex flex-col flex-grow gap-y-2 justify-between p-4 rounded-lg bg-custom-off-white">
         <label className="flex justify-between items-center w-full h-fit">
           <p className="text-sm text-custom-gray">First Name*</p>
-          <input
-            type="text"
-            name="firstName"
-            className="w-1/2 h-8 rounded-lg bg-white border-1.5 border-solid border-custom-light-gray outline-none focus:border-custom-purple px-2 text-sm text-custom-black"
-            autoComplete="off"
-            onChange={(e) => handleUserCredentialsUpdate(e)}
-          />
+          <input type="text" name="firstName" className="w-1/2 h-8 rounded-lg bg-white border-1.5 border-solid border-custom-light-gray outline-none focus:border-custom-purple px-2 text-sm text-custom-black" autoComplete="off" onChange={(e) => handleUserCredentialsUpdate(e)} />
         </label>
         <label className="flex justify-between items-center w-full h-fit">
           <p className="text-sm text-custom-gray">Last Name*</p>
-          <input
-            type="text"
-            name="lastName"
-            className="w-1/2 h-8 rounded-lg bg-white border-1.5 border-solid border-custom-light-gray outline-none focus:border-custom-purple px-2 text-sm text-custom-black"
-            autoComplete="off"
-            onChange={(e) => handleUserCredentialsUpdate(e)}
-          />
+          <input type="text" name="lastName" className="w-1/2 h-8 rounded-lg bg-white border-1.5 border-solid border-custom-light-gray outline-none focus:border-custom-purple px-2 text-sm text-custom-black" autoComplete="off" onChange={(e) => handleUserCredentialsUpdate(e)} />
         </label>
         <label className="flex justify-between items-center w-full h-fit">
           <p className="text-sm text-custom-gray">Email</p>
-          <input
-            type="text"
-            name="email"
-            className="w-1/2 h-8 rounded-lg bg-white border-1.5 border-solid border-custom-light-gray outline-none focus:border-custom-purple px-2 text-sm text-custom-black"
-            autoComplete="off"
-            onChange={(e) => handleUserCredentialsUpdate(e)}
-          />
+          <input type="text" name="email" className="w-1/2 h-8 rounded-lg bg-white border-1.5 border-solid border-custom-light-gray outline-none focus:border-custom-purple px-2 text-sm text-custom-black" autoComplete="off" onChange={(e) => handleUserCredentialsUpdate(e)} />
         </label>
       </section>
       <footer className="flex justify-end mt-auto w-full h-auto">
-        <button
-          type="submit"
-          className="flex justify-center items-center text-sm bg-custom-purple border-solid border-1.5 text-white font-semibold w-fit h-10 hover:bg-custom-light-purple px-5 gap-x-1  rounded-md"
-        >
+        <button type="submit" className="flex justify-center items-center text-sm bg-custom-purple border-solid border-1.5 text-white font-semibold w-fit h-10 hover:bg-custom-light-purple px-5 gap-x-1  rounded-md">
           Save
         </button>
       </footer>
