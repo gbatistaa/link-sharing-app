0b58ca095af7320548b4cdb5ecde25ffcdd20032
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-19 22:23:54 -0300

    deleting the dynamic styles and text and creating the keep-after-reloading file keeper logic

--- src/components/Menu/components/Profile/ProfileDetails.tsx
+++ src/components/Menu/components/Profile/ProfileDetails.tsx
@@ -1,4 +1,4 @@
-import { atom, useAtom } from "jotai";
+import { useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import React, { useEffect } from "react";
 import UploadImageIcon from "../../../../../assets/images/icon-upload-image";
@@ -6,11 +6,7 @@ import { ProfilePicture } from "../../../../ts/classes/ProfilePicture";
 import UserCredentials from "../../../../ts/classes/UserCredentials";
 
 // Atom para o estado da imagem
-const storageAtom = atomWithStorage<ProfilePicture>("profilePicture", { name: "", size: 0, type: "", url: null });
-
-const pictureAtom = atom<ProfilePicture>((get) => {
-  
-})
+const pictureAtom = atomWithStorage<ProfilePicture>("profilePicture", { file: { name: "", size: 0, type: "", content: "" } });
 
 // Atom para os dados do usuário
 export const userCredentialsAtom = atomWithStorage<UserCredentials>("userCredentials", {
@@ -26,19 +22,21 @@ function ProfileDetails(): JSX.Element {
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     if (event.target.files && event.target.files[0]) {
       const newProfilePicture = event.target.files[0];
-      const newProfilePictureUrl = URL.createObjectURL(newProfilePicture);
+      const reader = new FileReader();
 
-      setProfilePicture({
-        name: newProfilePicture.name,
-        size: newProfilePicture.size,
-        type: newProfilePicture.type,
-        url: newProfilePictureUrl,
-      });
+      reader.onload = () => {
+        const base64Data = reader.result as string;
 
-      console.log("Selected file:", newProfilePicture.name);
-      console.log("File size:", (newProfilePicture.size / 1024).toFixed(2), "KB");
-      console.log("File type:", newProfilePicture.type);
-      console.log("File url:", newProfilePictureUrl);
+        setProfilePicture({
+          file: {
+            name: newProfilePicture.name,
+            size: newProfilePicture.size,
+            type: newProfilePicture.type,
+            content: base64Data,
+          },
+        });
+      };
+      reader.readAsDataURL(newProfilePicture);
     } else {
       console.log("Nenhum arquivo selecionado.");
     }
@@ -52,25 +50,6 @@ function ProfileDetails(): JSX.Element {
     }));
   };
 
-  const backgroundImageStyle = React.useMemo(() => {
-    const storedPicture = localStorage.getItem("profilePicture");
-
-    if (storedPicture) {
-      try {
-        const parsedPicture = JSON.parse(storedPicture);
-        return parsedPicture.url ? `url(${parsedPicture.url})` : undefined;
-      } catch (error) {
-        console.error("Erro ao parsear o profilePicture:", error);
-        return undefined;
-      }
-    }
-
-    return undefined;
-  }, []);
-  const fillColor = profilePicture.url ? "#ffffff" : "#633cff";
-  const buttonText = profilePicture.url ? "Change Image" : "+Upload Image";
-  const textColor = profilePicture.url ? "text-white" : "text-custom-purple";
-
   useEffect(() => {
     console.log("Profile picture state loaded:", profilePicture);
   }, [profilePicture]);
@@ -83,20 +62,18 @@ function ProfileDetails(): JSX.Element {
         <p className="text-sm text-custom-gray">Profile Picture</p>
         <label
           htmlFor="profilePicture"
-          className={`flex flex-col bg-center bg-cover gap-y-2 justify-center items-center h-full rounded-lg cursor-pointer aspect-square font-semibold text-sm ${profilePicture.url ? "bg-cover" : "bg-custom-lavender"}`}
+          className={`flex flex-col bg-center bg-cover gap-y-2 justify-center items-center h-full rounded-lg cursor-pointer aspect-square font-semibold text-sm ${profilePicture.file.content ? "bg-cover" : "bg-custom-lavender"}`}
           style={{
-            backgroundImage: backgroundImageStyle,
+            backgroundImage: profilePicture.file.content ? `url(${profilePicture.file.content})` : undefined,
           }}
         >
-          <UploadImageIcon fillColor={fillColor} />
-          <p className={textColor}>{buttonText}</p>
+          <UploadImageIcon fillColor={`${profilePicture.file.content ? "#ffffff" : "#633cff"}`} />
+          <p className={`${profilePicture.file.content ? "text-white" : "text-custom-purple"}`}>{`${profilePicture.file.content ? "Change Image" : "+Upload Image"}`}</p>
           <input type="file" id="profilePicture" className="hidden" onChange={(e) => handleImageChange(e)} />
         </label>
         <div className="flex flex-col">
           <p className="text-xs text-custom-gray">Image must be below 1024x1024.</p>
           <p className="text-xs text-custom-gray">Use PNG or JGP format.</p>
-
-          <p className="text-xs text-custom-gray">{`File: ${profilePicture.name}, Size: ${(profilePicture.size / 1024).toFixed(2)} KB, Type: ${profilePicture.type} ${localStorage.getItem("profilePicture")}`}</p>
         </div>
       </section>
       <section className="flex flex-col flex-grow gap-y-2 justify-between p-4 rounded-lg bg-custom-off-white">
