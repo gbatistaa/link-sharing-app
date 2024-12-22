3596e12f85058665cef3c1c3ce0f0a82d79cf792
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-22 15:36:48 -0300

    sincronizing the platform name with its color palette bg color

--- src/components/Menu/LinkCard.tsx
+++ src/components/Menu/LinkCard.tsx
@@ -1,14 +1,17 @@
 import { useAtom } from "jotai";
+import { cardColorSelector } from "../../ts/logic/ColorSelectors";
 import { linksAtomFamily } from "./Menu";
 import KeyValuePlataforms from "./components/Links/KeyValuePlataforms";
 
 function LinkCard({ linkId }: { linkId: number }): JSX.Element {
   const [link] = useAtom(linksAtomFamily(linkId));
 
+  const linkCardBgColor = cardColorSelector(link.plataform);
+
   return (
-    <label htmlFor="" className="bg-blue-200 w-11/12 h-3/20 rounded-lg">
+    <label htmlFor="" className={`${linkCardBgColor} w-11/12 h-3/20 rounded-lg`}>
       <div>
-        <KeyValuePlataforms plataform={link.plataform} selected={false} />
+        <KeyValuePlataforms plataform={link.plataform} colorState={"card"} />
       </div>
     </label>
   );
