79109474eadc1eda5c3a3781097c1c141d5c4cec
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-23 07:28:58 -0300

    changing the border radius strength and plataform text and arrow

--- src/components/Menu/LinkCard.tsx
+++ src/components/Menu/LinkCard.tsx
@@ -1,4 +1,5 @@
 import { useAtom } from "jotai";
+import arrow from "../../../assets/images/icon-arrow-right.svg";
 import { cardColorSelector } from "../../ts/logic/ColorSelectors";
 import { linksAtomFamily } from "./Menu";
 import KeyValuePlataforms from "./components/Links/KeyValuePlataforms";
@@ -9,10 +10,12 @@ function LinkCard({ linkId }: { linkId: number }): JSX.Element {
   const linkCardBgColor = cardColorSelector(link.plataform);
 
   return (
-    <label htmlFor="" className={`${linkCardBgColor} w-11/12 h-3/20 rounded-lg`}>
-      <div>
+    <label htmlFor="" className={`${linkCardBgColor} w-11/12 h-3/20 rounded-md flex justify-between items-center cursor-pointer`}>
+      <div className="flex flex-row justify-between items-center gap-x-4">
         <KeyValuePlataforms plataform={link.plataform} colorState={"card"} />
+        <p className="text-white">{link.plataform}</p>
       </div>
+      <img src={arrow} alt="arrow" />
     </label>
   );
 }
