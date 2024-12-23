0a52c89f25892cb7e81ab566f29c4a7964bed097
gbatistaa <gabrielbatistabarbosa4@gmail.com>
2024-12-23 07:30:54 -0300

    eslint

--- src/components/Menu/components/Links/Empty.tsx
+++ src/components/Menu/components/Links/Empty.tsx
@@ -5,10 +5,7 @@ function Empty(): JSX.Element {
     <div className="bg-custom-off-white flex flex-auto justify-center items-center flex-col rounded-lg gap-y-6">
       <img src={empty} />
       <h1 className="text-3xl font-bold">Let's get you started</h1>
-      <p className="text-center w-3/5 text-sm text-custom-gray">
-        Use the "Add new link" button to get started. Once you have more than one link, you can reorder and edit them.
-        We're here to help you share your profiles with everyone!
-      </p>
+      <p className="text-center w-3/5 text-sm text-custom-gray">Use the "Add new link" button to get started. Once you have more than one link, you can reorder and edit them. We're here to help you share your profiles with everyone!</p>
     </div>
   );
 }
